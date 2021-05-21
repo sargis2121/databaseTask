99f58cba3bc6f02edea7d3dbc8b078b9ef053512
Sargis2121 <saq2121y@gmail.com>
2021-05-21 08:10:48 -0800

    using routes for dinamism

--- index.js
+++ index.js
@@ -1,70 +1,12 @@
 const mysql = require("mysql2");
 const express = require("express");
-const { json } = require("body-parser");
 const bodyParser = require("body-parser");
+const routes = require("./rout");
 
 const app = express();
+const port = 8086;
 
-app.use(bodyParser.json());
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "usersdb2",
-  password: "123456",
-});
-connection.connect(function (err) {
-  if (err) {
-    return console.error(err.message);
-  } else {
-    console.log("connected");
-  }
-});
-app.get("/api/users", (req, res) => {
-  connection.query("SELECT * FROM users", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.send(data);
+app.use(routes);
+app.listen(port, function () {
+    console.log("The server is waiting for a connection ...");
   });
-});
-app.post("/api/create", (req, res) => {
-  const people = {
-    id: req.params.id,
-    name: req.body.name,
-    age: req.body.age,
-  };
-  connection.query("INSERT INTO users SET ? ", people, (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.send(data);
-  });
-});
-app.post("/api/edit", (req, res) => {
-  if (!req.body) {
-    return res.sendStatus(400);
-  }
-  const name = req.body.name;
-  const age = req.body.age;
-  const id = req.body.id;
-
-  connection.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.send(data);
-  });
-});
-app.post("/api/delete/:id", (req, res) => {
-  const id = req.params.id;
-  connection.query("DELETE FROM users WHERE id=?", [id], (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.send(data);
-  });
-});
-
-app.listen(8086, function () {
-  console.log("The server is waiting for a connection ...");
-});
